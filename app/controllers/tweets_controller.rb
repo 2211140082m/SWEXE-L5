56728903c7dff34638e1c07b36b2e9be56d6ff74
Ubuntu <ubuntu@ip-172-31-34-180.ec2.internal>
2024-10-22 11:02:05 +0000

    first commit

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,23 @@
+class TweetsController < ApplicationController
+    def index
+        @tweets = Tweet.all
+    end
+    
+    def new
+        @tweet = Tweet.new
+    end
+    
+    def create
+        logger.debug "-" * 50
+        logger.debug session[:login_uid]
+        t = Tweet.new(message: params[:tweet][:message])
+        t.user = User.find_by(uid: session[:login_uid])
+        t.save
+        redirect_to tweets_path
+    end
+    
+    def destroy
+        Tweet.find(params[:id]).destroy
+        redirect_to tweets_path
+    end
+end
